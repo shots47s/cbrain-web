6d8a4dc7a8cd3860b6498bd9cacdb76cde786f89
candicecz <czech.candice@gmail.com>
2019-02-25 13:05:49 -0500

    [ADD] Get-Started desktop + responsive

---
+++ src/get-started/get-started.js
@@ -0,0 +1,16 @@
+import "./get-started.scss";
+
+const selectResource = (e) => {
+  const resource_els = Array.from(document.querySelectorAll("[data-option]"));
+  return resource_els.map((_) => {
+    _.dataset.option === e.currentTarget.dataset.option
+      ? (_.dataset.selected = "true")
+      : (_.dataset.selected = "false");
+  });
+};
+
+Array.from(
+  document.querySelectorAll(".resource-option"),
+).map((_) => {
+  _.addEventListener("click", selectResource);
+});
--- webpack.common.config.js
+++ webpack.common.config.js
@@ -10,6 +10,7 @@ module.exports = {
     about: "./src/about/about.js",
     projects: "./src/projects/projects.js",
     features: "./src/features/features.js",
+    getStarted: "./src/get-started/get-started.js",
   },
   output: {
     filename: "js/[name].[hash].bundle.js",
@@ -77,6 +78,11 @@ module.exports = {
       filename: "features.html",
       template: "src/features/features.html",
       chunks: ["features"],
+    }),
+    new HtmlWebpackPlugin({
+      filename: "get-started.html",
+      template: "src/get-started/get-started.html",
+      chunks: ["getStarted"],
     }),
     new CopyWebpackPlugin([{ from: "src/assets", to: "assets" }]),
   ],

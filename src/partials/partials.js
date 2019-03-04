48a1bd2c9e859b4cf75dfe07f300ccee13635bb8
candicecz <czech.candice@gmail.com>
2019-03-04 14:23:11 -0500

    [FIX] Navigation active state

--- src/partials/partials.js
+++ src/partials/partials.js
@@ -5,10 +5,11 @@
 */
 
 const setSelectedLink = () => {
-  const selection = window.location.pathname.split("/")[1].split(".")[0];
+  const path = window.location.pathname.split("/");
+  const selection = path[path.length - 1].split(".")[0];
   const selected_link = document.querySelector(`[data-page='${selection}']`);
   if (selected_link) {
-    selected_link.dataset.selected = true;
+    selected_link.dataset.active = true;
   }
 };
 
